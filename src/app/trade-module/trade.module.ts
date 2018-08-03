752ecc1c7e4f9716c52208a675e29f799e9b35eb
valeraGorobets <valera.gorobets@gmail.com>
2018-08-03 15:16:33 +0300

    Change project file structure

--- src/app/trade-module/trade.module.ts
+++ src/app/trade-module/trade.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CryptoExchangeModule } from '../crypto-exchange-module/crypto-exchange.module';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [],
+  declarations: [CryptoExchangeModule],
 })
 export class TradeModule { }
