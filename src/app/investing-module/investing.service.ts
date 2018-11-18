a65d8bf56f4ba0f88377346e1d67b8ff3668afa8
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2018-11-18 21:04:08 +0300

    Add check for shouldBeClosedEarly position

--- src/app/investing-module/investing.service.ts
+++ src/app/investing-module/investing.service.ts
@@ -1,6 +1,7 @@
 import { Candle, NotificationCandle } from '../models/Candle';
 import { CandlesChartFormat } from '../models/ChartFormats/CandlesChartFormat';
 import { concatMap, delay } from 'rxjs/operators';
+import { first } from 'rxjs/operators';
 import { HitbtcApi } from '../crypto-exchange-module/hitbtc-api';
 import { Injectable } from '@angular/core';
 import { InjectableObservables } from '../app-module/injectable-observables';
@@ -19,17 +20,14 @@ import { Strategy } from './strategies/abstractStrategy';
 export class InvestingService{
   private strategy: Strategy;
   private strategyId = 'MACDFromPrimarySymbolStrategy';
-  private openedTrade = {
-  time: "2018-11-18T17:13:00.000Z",
-  openPrice: 0.03148400000908,
-  value: 31762.164909160205,
-};
+  private openedTrade = null;
   private money: number = 1000;
   private config = {
     investingSymbol: 'ETHBTC',
     indicatorSymbol: 'BTCUSD',
     quantityIncrement: 0.001,
     tickSize: 0.000001,
+    allowedLost: 0.001,
   }
 
   constructor(
@@ -55,36 +53,48 @@ export class InvestingService{
 
   private handleActionUpdate(action: {time: string, side: Side}) {
     console.log(action);
-    if (action.side === Side.buy && !this.openedTrade) {
-      this.openPosition(action.time);
-    } else if (action.side === Side.sell && this.openedTrade) {
-      this.closePosition(action.time);
-    }
-  }
-
-  private openPosition(time: string): void {
-    console.log('Open: ' + time);
-    this.hitbtcApiService.getOrderbook(this.config.investingSymbol).subscribe((orderbook: Orderbook) => {
+    this.hitbtcApiService.getOrderbook(this.config.investingSymbol).pipe(
+      first(),
+    ).subscribe((orderbook: Orderbook) => {
       console.log(orderbook.bid[0].price + ' bid | ask ' + orderbook.ask[0].price);
-      const openPrice = +orderbook.ask[0].price + 2 * this.config.tickSize;
-      this.openedTrade = {
-        time,
-        openPrice,
-        value: this.money / openPrice,
+      if (action.side === Side.buy && !this.openedTrade) {
+        this.openPosition(action.time, +orderbook.bid[0].price);
+      } else if (action.side === Side.sell && this.openedTrade) {
+        this.closePosition(action.time, +orderbook.ask[0].price);
+      } else if (this.shouldBeClosedEarly(+orderbook.ask[0].price)) {
+        this.closePosition(action.time, +orderbook.ask[0].price);
       }
-      console.log(this.openedTrade);
     });
   }
 
-  private closePosition(time: string): void {
+  private openPosition(time: string, bidPrice: number): void {
+    console.log('Open: ' + time);
+    const openPrice = bidPrice - 2 * this.config.tickSize;
+    this.openedTrade = {
+      time,
+      openPrice,
+      value: this.money / openPrice,
+    }
+    console.log(this.openedTrade);
+  }
+
+  private closePosition(time: string, askPrice: number): void {
     console.log('Close: ' + time);
-    this.hitbtcApiService.getOrderbook(this.config.investingSymbol).subscribe((orderbook: Orderbook) => {
-      console.log(orderbook.bid[0].price + ' bid | ask ' + orderbook.ask[0].price);
-      const closePrice = +orderbook.bid[0].price - 2 * this.config.tickSize;
-      this.money = this.openedTrade.value * closePrice;
-      this.openedTrade = null;
-      console.log(this.money);
-    });
+    const closePrice = askPrice + 2 * this.config.tickSize;
+    this.money = this.openedTrade.value * closePrice;
+    this.openedTrade = null;
+    console.log(this.money);
+  }
+
+  private shouldBeClosedEarly(askPrice: number): boolean {
+    if (!this.openedTrade) {
+      return false;
+    }
+    const openPrice = this.openedTrade.openPrice;
+    if ((openPrice - askPrice)/openPrice > this.config.allowedLost) {
+      return true;
+    }
+    return true;
   }
 
   public stopWatching(): void {
