4bd6628b468040737b0782b7cb29c72ffd1fbea2
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2018-11-18 11:44:17 +0300

    Remove unused constructor

--- src/app/investing-module/strategies/abstractStrategy.ts
+++ src/app/investing-module/strategies/abstractStrategy.ts
@@ -3,10 +3,6 @@ import { Candle } from '../../models/Candle';
 import { ReplaySubject } from 'rxjs';
 
 export abstract class Strategy {
-  constructor(
-    public indicator$: any,
-  ) {}
-
   public abstract advisedInvestingSide(candles: Candle[], isPartOfStrategy?: boolean): Side;
   public abstract notifyAboutNewIndicatorValues(params: any): void;
 }
